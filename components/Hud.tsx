7c1a5c83dda48a0eb3292ceb4ab143dd5d99bc34
lullwood-founding-engineer <founding-engineer@lullwood.invalid>
2026-09-07 07:30:34 +0300

    Remove unused useCallback import

--- components/Hud.tsx
+++ components/Hud.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import DesktopControls from './DesktopControls';
 import MobileControls from './MobileControls';
 import OrientationGate from './OrientationGate';
